9b7a9946828465e895351a5c3149dc9375e5845f
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-11-28 18:48:46 +0100

    implement get blog

--- api/routes/blog.py
+++ api/routes/blog.py
@@ -1,9 +1,11 @@
 import datetime
 
+from bson import ObjectId
 from fastapi import APIRouter
 
 from config.config import blog_collections
 from models.blog_model import BlogModel
+from serializer.blog import DecodeBlog, DecodeBlogs
 
 blog_root = APIRouter(tags=["blog"])
 
@@ -18,3 +20,17 @@ def post_blog(blog: BlogModel):
     doc_id = str(new_blog.inserted_id)
 
     return {"res": "ok", "message": "blog posted successfully", "id": doc_id}
+
+
+@blog_root.get("/get/{_id}")
+def get_blog_by_id(_id: str):
+    res = blog_collections.find_one({"_id": ObjectId(_id)})
+    decoded_blog = DecodeBlog(res)
+    return {"status": "ok", "data": decoded_blog}
+
+
+@blog_root.get("/all/blogs")
+async def get_all_blogs():
+    res = blog_collections.find()
+    decoded_blog = DecodeBlogs(res)
+    return {"status": "ok", "data": decoded_blog, "other_data": res}
--- serializer/blog.py
+++ serializer/blog.py
@@ -5,12 +5,13 @@ def DecodeBlog(doc) -> dict:
     return {
         "_id": str(doc["_id"]),
         "title": doc["title"],
-        "sub_title": doc["sub_title"],
+        "subtitle": doc["subtitle"],
         "content": doc["content"],
         "author": doc["author"],
-        "date": doc["date"],
+        "date": doc["current_date"],
     }
+
 
 # all blogs
 def DecodeBlogs(docs) -> list:
-    return[DecodeBlog(doc) for doc in docs]
+    return [DecodeBlog(doc) for doc in docs]
