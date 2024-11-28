152e834ed501d9053c05164f05542778cfac1161
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-11-28 16:51:34 +0100

    set up mongodb

---
+++ config/config.py
@@ -0,0 +1,22 @@
+import os
+
+import dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+dotenv.load_dotenv()
+
+
+client = MongoClient(os.getenv("MONGO_URL"), server_api=ServerApi("1"))
+""
+
+db = client.blogging
+collections = db["blogs"]
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command("ping")
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+except Exception as e:
+    print(e)
